138c4819007a62104b8a1c6695cbe20e86118d86
gadeaagustin <gadea.agustin@gmail.com>
2017-05-09 13:50:45 -0300

    Envio de correo instantaneo hecho

---
+++ app/src/main/java/com/gibio/bt_graph/ConfigMail.java
@@ -0,0 +1,10 @@
+package com.gibio.bt_graph;
+
+/**
+ * Created by Agustin on 5/5/2017.
+ */
+
+public class ConfigMail {
+    public static final String EMAIL ="gibio.pacientes@gmail.com";
+    public static final String PASSWORD ="grupogibio";
+}
